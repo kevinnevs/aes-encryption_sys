2ade6206c69b972c689632a890471de9047ff6fc
Kevin Njuguna <kevinnjuguna2@gmail.com>
2023-02-13 21:40:27 +0300

    initial commit

---
+++ aes-encryption_sys-app/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function FileInput() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  return (
+    <div className='file-input'>
+      <input type="file" onChange={handleFileChange} />
+      {selectedFile ? (
+        <p>{selectedFile.name}</p>
+      ) : (
+        <p>No file selected</p>
+      )}
+    </div>
+  );
+}
+
+export default FileInput;
