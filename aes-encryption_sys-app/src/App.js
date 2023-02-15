84b11d2f514f7607b9a4c79d5258125d512bba4d
Kevin Njuguna <kevinnjuguna2@gmail.com>
2023-02-15 21:37:06 +0300

    initial commit

--- aes-encryption_sys-app/src/App.js
+++ aes-encryption_sys-app/src/App.js
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Login } from "./Login";
 import { Register } from "./Register";
+import FileUpload from './FileUpload/FileUpload';
+import FileList from './FileList/FileList';
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
+  const [files, setFiles] = useState([])
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
+  }
+
+  const removeFile = (filename) => {
+    setFiles(files.filter(file => file.name !== filename))
   }
 
   return (
@@ -16,6 +23,10 @@ function App() {
       {
         currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
       }
+      <div className="title">Upload file</div>
+      <FileUpload files={files} setFiles={setFiles}
+        removeFile={removeFile} />
+      <FileList files={files} removeFile={removeFile} />
     </div>
   );
 }
