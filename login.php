7b6a1f7343dd8c105ae85cfca1c40001b6e85972
kevinnevs <kevinnjuguna2@gmail.com>
2023-03-07 12:22:50 +0300

    including water css style

--- login.php
+++ login.php
@@ -47,6 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 <body>
     
